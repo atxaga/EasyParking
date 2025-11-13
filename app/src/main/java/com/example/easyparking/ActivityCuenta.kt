651dd08d869e408b13a07d195bbe6d4660cc0735
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-11-13 12:53:02 +0100

    Cuenta

---
+++ app/src/main/java/com/example/easyparking/ActivityCuenta.kt
@@ -0,0 +1,11 @@
+package com.example.easyparking
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityCuenta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cuenta)
+    }
+}
